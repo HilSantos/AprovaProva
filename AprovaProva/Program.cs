5cb91ba2ff0af295387704228d6245e55cb041b4
hilton.lssantos <hilton.lssantos@RIP0576973W10-1>
2024-10-09 17:26:12 -0300

    Adicionar arquivos de projeto.

---
+++ AprovaProva/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AprovaProva
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal nota;
+
+            Console.WriteLine("Informe a nota1: ");
+            nota = Convert.ToDecimal(Console.ReadLine());
+
+            if (nota >= 6)
+            {
+                Console.WriteLine("Aprovado!");
+            }
+            else
+            {
+                Console.WriteLine("Reprovado");
+            }
+            Console.ReadKey();
+        }
+    }
+}
